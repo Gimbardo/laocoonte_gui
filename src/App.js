85376df7c5fc79e2f309dca8029aae62801e0e08
Gimbardo <gamberoatomico@gmail.com>
2022-04-14 12:24:22 +0200

    #1 :tada: first test-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Clock from './components/Clock'
+import SearchButton from './components/SearchButton'
+
+const element = <h1>Hello, world</h1>;
+
+function Logo(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element2 = <Logo name="Logo" />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo name="cagasburra"/>
+      <Clock />
+      <SearchButton />
     </div>
   );
 }
